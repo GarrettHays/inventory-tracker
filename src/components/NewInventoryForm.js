cbe6e76671f2fe747cb91212786e32bce00d1f75
Garrett Hays <GarrettlHays@gmail.com>
2022-09-09 12:54:47 -0700

    adds new inventory form to project

---
+++ src/components/NewInventoryForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { v4 } from 'uuid';
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+
+
+function NewInventoryForm(props) {
+
+  function handleNewInventoryFormSubmission(event){
+    event.preventDefault();
+    props.onNewInventoryCreation({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, weight: event.target.weight.value, description: event.target.description.value, quantity: event.target.quantity.value, id: v4()});
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewInventoryFormSubmission}
+        buttonText="Submit"
+      />
+    </React.Fragment>
+  );
+}
+
+NewInventoryForm.propTypes = {
+  onNewInventoryCreation: PropTypes.func
+};
+
+export default NewInventoryForm;
