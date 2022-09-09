3957f64916f2b631d69d3ffce02f389f3244bf45
Garrett Hays <GarrettlHays@gmail.com>
2022-09-09 12:54:11 -0700

    adds inventory detail state slice to project

---
+++ src/components/InventoryDetail.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function InventoryDetail(props) {
+  const { inventory, onClickingDelete, onClickingDecrement } = props;
+
+  return (
+    <React.Fragment>
+          <h1>Inventory Detail</h1>
+          <h3>Brand: {inventory.brand} - Name: {inventory.name}</h3>
+          <p><em>${inventory.price}</em> - <em>Weight {inventory.weight} lbs</em></p>
+          <p>Description: {inventory.description}</p>
+          <p>{inventory.quantity} quantity remaining</p>
+          <button className='buttonStyle' onClick={ props.onClickingEdit }>Update Inventory</button>
+          <button className='buttonStyle' onClick={() => onClickingDelete(inventory.id) }>Remove Inventory</button>
+          <button className='buttonStyle' onClick={() => onClickingDecrement(inventory.id) }>Sell one piece of inventory</button>
+          <hr/>
+    </React.Fragment>
+  );
+}
+
+InventoryDetail.propTypes = {
+  inventory: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func,
+  onClickingDecrement: PropTypes.func
+}
+
+export default InventoryDetail;
